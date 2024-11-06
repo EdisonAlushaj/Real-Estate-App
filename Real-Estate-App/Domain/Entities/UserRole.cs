e7ddcf05f88f92ada6aa97344c971c4715e8c571
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2024-11-06 01:14:22 +0100

    Sell Table & User
    Lidhja e Sell table me user table dhe konfiguirimi i tyre.

--- Real-Estate-App/Domain/Entities/UserRole.cs
+++ Real-Estate-App/Domain/Entities/UserRole.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class UserRole
+    public class UserRole : IdentityUser
     {
         public string Username { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
